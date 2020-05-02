5a35d9c616ae33757f7358ab1375e963bc64d613
Petar Trifunovic <petar.t@elfak.rs>
2020-05-02 14:46:25 +0200

    Napravljena hijerarhija Radnik

---
+++ SBPZelenePovrsine/SBPZelenePovrsine/Entiteti/Radnik.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SBPZelenePovrsine.Entiteti
+{
+    public abstract class Radnik
+    {
+        public virtual string BrRadneKnjizice { get; set; }
+        public virtual string MBr { get; set; }
+        public virtual string Ime { get; set; }
+        public virtual string ImeRoditelja { get; set; }
+        public virtual string Prezime { get; set; }
+        public virtual string Adresa { get; set; }
+        public virtual DateTime DatumRodjenja { get; set; }
+        public virtual string StrucnaSprema { get; set; }
+    }
+
+    public class RadnikOdrzavanjeZelenila: Radnik
+    {
+  
+    }
+
+    public class RadnikOdrzavanjeHigijene: Radnik
+    {
+
+    }
+
+    public class RadnikOdrzavanjeObjekataUParku: Radnik
+    {
+
+    }
+}
---
+++ SBPZelenePovrsine/SBPZelenePovrsine/Mapiranja/RadnikMapiranja.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+using SBPZelenePovrsine.Entiteti;
+
+namespace SBPZelenePovrsine.Mapiranja
+{
+    public class RadnikMapiranja: ClassMap<Radnik>
+    {
+        public RadnikMapiranja()
+        {
+            Table("RADNIK");
+
+            DiscriminateSubClassesOnColumn("TIP_ANGAZOVANJA");
+
+            Id(x => x.BrRadneKnjizice, "BR_RADNE_KNJIZICE").GeneratedBy.Assigned();
+
+            Map(x => x.MBr, "MBR"); //Da li treba dodati Unique, radi sigurnosti?
+            Map(x => x.Ime, "IME");
+            Map(x => x.ImeRoditelja, "IME_RODITELJA");
+            Map(x => x.Prezime, "PREZIME");
+            Map(x => x.Adresa, "ADRESA");
+            Map(x => x.DatumRodjenja, "DATUM_RODJENJA");
+            Map(x => x.StrucnaSprema, "STRUCNA_SPREMA");
+        }
+    }
+
+    public class RadnikOdrzavanjeZelenilaMapiranja: SubclassMap<RadnikOdrzavanjeZelenila>
+    {
+        public RadnikOdrzavanjeZelenilaMapiranja()
+        { 
+            DiscriminatorValue("Održavanje zelenila");
+        }
+    }
+
+    public class RadnikOdrzavanjeHigijeneMapiranja : SubclassMap<RadnikOdrzavanjeHigijene>
+    {
+        public RadnikOdrzavanjeHigijeneMapiranja()
+        {
+            DiscriminatorValue("Održavanje higijene");
+        }
+    }
+
+    public class RadnikOdrzavanjeObjekataUParkuMapiranja : SubclassMap<RadnikOdrzavanjeObjekataUParku>
+    {
+        public RadnikOdrzavanjeObjekataUParkuMapiranja()
+        {
+            DiscriminatorValue("Održavanje objekata u parku");
+        }
+    }
+}
